c65c3fd825da26d133d7501655678be1ce2a52f6
Rozhkin Michael <rozhkin.m@gmail.com>
2024-09-29 21:32:57 +0300

    Спринт 1 настроил админку внутренними инструментами django

--- movies_admin/movies/admin.py
+++ movies_admin/movies/admin.py
@@ -14,12 +14,19 @@ class PersonFilmWorkInline(admin.TabularInline):
 class FilmWorkAdmin(admin.ModelAdmin):
     inlines = (GenreFilmWorkInline, PersonFilmWorkInline)
 
+    list_display = ('title', 'type', 'creation_date', 'rating',)
+    list_filter = ('type', 'creation_date', 'rating',)
+    search_fields = ('title', 'description', 'id', 'creation_date',)
+
 
 @admin.register(Genre)
 class GenreAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name', 'description',)
+    list_filter = ('name',)
+    search_fields = ('name', 'description', 'id',)
 
 
 @admin.register(Person)
 class PersonAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('full_name',)
+    search_fields = ('full_name', 'id',)
