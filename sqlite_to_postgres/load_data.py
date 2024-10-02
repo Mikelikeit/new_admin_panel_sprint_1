a53176634c32329b516de2a8b2a5442cb7d87e09
Rozhkin Michael <rozhkin.m@gmail.com>
2024-10-02 20:11:19 +0300

    Спринт 1 Проектное задание Перенос данных первый рабочий вариант

--- sqlite_to_postgres/load_data.py
+++ sqlite_to_postgres/load_data.py
@@ -5,6 +5,7 @@ from dataclasses import dataclass, astuple, asdict, fields
 from datetime import datetime
 from typing import Generator
 from uuid import UUID
+from enum import Enum
 
 import psycopg
 from psycopg import ClientCursor
@@ -83,41 +84,55 @@ table_fabric = {
 }
 
 
+class DifferentColumn(Enum):
+    created = 'created_at'
+    modified = 'updated_at'
+
+
 def extract_data(sqlite_cursor: sqlite3.Cursor,
                  table_name: str, column_names: str) -> Generator[list[sqlite3.Row], None, None]:
     query_slt_sqlite = f'SELECT {column_names} FROM {table_name}'
-    for replace in (('created', 'created_at'), ('modified', 'updated_at')):
-        query_slt_sqlite = query_slt_sqlite.replace(*replace)
+    for replace in DifferentColumn:
+        query_slt_sqlite = query_slt_sqlite.replace(replace.name, replace.value)
     sqlite_cursor.execute(query_slt_sqlite)
     while results := sqlite_cursor.fetchmany(BATCH_SIZE):
         yield results
 
 
+def _rename_column(row: dict) -> dict:
+    for name in DifferentColumn:
+        if row.get(name.value):
+            row[name.name] = row.get(name.value)
+            del row[name.value]
+    return row
+
+
 def transform_data(sqlite_cursor: sqlite3.Cursor,
                    table_name: str, column_names: str) -> Generator[list[dataclass], None, None]:
     for batch in extract_data(sqlite_cursor, table_name, column_names):
-        yield [table_fabric[table_name](**dict(row)) for row in batch]
+        yield [table_fabric[table_name](**_rename_column(dict(row))) for row in batch]
 
 
-def load_data(sqlite_cursor: sqlite3.Cursor, pg_cursor: psycopg.Cursor, table_name: str) -> None:
+def load_data(sqlite_cursor: sqlite3.Cursor, pg_cursor: ClientCursor, table_name: str) -> None:
     """Основной метод загрузки данных из SQLite в Postgres"""
     pg_column_names = [field.name for field in fields(table_fabric[table_name])]
-    miss = ['created_at', 'updated_at']
-    column_names_str = ','.join(pg_column_names)
-    col_count = ', '.join(['%s'] * len(pg_column_names))
+    column_names_str = ', '.join(pg_column_names)
+    placeholder = ', '.join(['%s'] * len(pg_column_names))
 
     for batch in transform_data(sqlite_cursor, table_name, column_names_str):
+        bind_values = ', '.join(
+            pg_cursor.mogrify(f'({placeholder})', astuple(item)) for item in batch
+        )
         query_insert_to_pg = (f'INSERT INTO content.{table_name} ({column_names_str}) '
-                              f'VALUES ({col_count}) ON CONFLICT (id) DO NOTHING;')
-        batch_as_tuples = [astuple(student) for student in batch]
-        pg_cursor.executemany(query_insert_to_pg, batch_as_tuples)
+                              f'VALUES {bind_values} ON CONFLICT (id) DO NOTHING;')
+        pg_cursor.execute(query_insert_to_pg)
 
 
 def get_all_table_names_sqlite(cursor: sqlite3.Cursor) -> list[str]:
     """Метод получения названий таблиц из SQLite"""
     query = "SELECT name FROM sqlite_master WHERE type='table';"
     cursor.execute(query)
-    return [tbl['name'] for tbl in cursor.fetchall()]
+    return sorted([tbl['name'] for tbl in cursor.fetchall()])
 
 
 if __name__ == '__main__':
@@ -127,11 +142,10 @@ if __name__ == '__main__':
         sqlite_conn.row_factory = sqlite3.Row
 
         with closing(sqlite_conn.cursor()) as sqlite_cur, closing(pg_conn.cursor(row_factory=dict_row)) as pg_cur:
-
             table_names_sqlite = get_all_table_names_sqlite(sqlite_cur)
-
             if not table_names_sqlite:
                 raise ValueError('В SQLite не было найдено ни одной таблицы для миграции')
             for tbl_name in table_names_sqlite:
                 load_data(sqlite_cur, pg_cur, tbl_name)
-                exit(22)
+                pg_conn.commit()
+                print(f'Таблица {tbl_name} залита')
