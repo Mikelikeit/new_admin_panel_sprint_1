dc9236bb8a2364af08f61bbba23d651946908817
Rozhkin Michael <rozhkin.m@gmail.com>
2024-10-03 00:15:53 +0300

    Спринт 1 Проектное задание Перенос данных итоговый результат без логирования и обработки ошибок

--- sqlite_to_postgres/load_data.py
+++ sqlite_to_postgres/load_data.py
@@ -66,6 +66,14 @@ class GenreFilmWork(UUIDMixin):
     film_work_id: UUID
     created: datetime
 
+    def __post_init__(self):
+        if isinstance(self.id, str):
+            self.id = UUID(self.id)
+        if isinstance(self.genre_id, str):
+            self.genre_id = UUID(self.genre_id)
+        if isinstance(self.film_work_id, str):
+            self.film_work_id = UUID(self.film_work_id)
+
 
 @dataclass
 class PersonFilmWork(UUIDMixin):
@@ -73,6 +81,14 @@ class PersonFilmWork(UUIDMixin):
     film_work_id: UUID
     role: str
     created: datetime
+
+    def __post_init__(self):
+        if isinstance(self.id, str):
+            self.id = UUID(self.id)
+        if isinstance(self.person_id, str):
+            self.person_id = UUID(self.person_id)
+        if isinstance(self.film_work_id, str):
+            self.film_work_id = UUID(self.film_work_id)
 
 
 table_fabric = {
@@ -89,28 +105,59 @@ class DifferentColumn(Enum):
     modified = 'updated_at'
 
 
+def _replace_column_name(query: str) -> str:
+    """Метод замены имен колонок в sql запросе"""
+    upd_q = query
+    for replace in DifferentColumn:
+        upd_q = upd_q.replace(replace.name, replace.value)
+    return upd_q
+
+
 def extract_data(sqlite_cursor: sqlite3.Cursor,
                  table_name: str, column_names: str) -> Generator[list[sqlite3.Row], None, None]:
+    """Метод получения данных из SQLite"""
     query_slt_sqlite = f'SELECT {column_names} FROM {table_name}'
-    for replace in DifferentColumn:
-        query_slt_sqlite = query_slt_sqlite.replace(replace.name, replace.value)
-    sqlite_cursor.execute(query_slt_sqlite)
+
+    sqlite_cursor.execute(_replace_column_name(query_slt_sqlite))
     while results := sqlite_cursor.fetchmany(BATCH_SIZE):
         yield results
 
 
-def _rename_column(row: dict) -> dict:
+def _reform_data(row: dict) -> dict:
+    """Метод точечного преобразования данных, адаптирование полей SQLite"""
     for name in DifferentColumn:
-        if row.get(name.value):
-            row[name.name] = row.get(name.value)
-            del row[name.value]
+        if not name.value in row.keys():
+            continue
+        if isinstance(row[name.value], str):
+            row[name.name] = datetime.fromisoformat(row.get(name.value))
+        del row[name.value]
     return row
 
 
 def transform_data(sqlite_cursor: sqlite3.Cursor,
                    table_name: str, column_names: str) -> Generator[list[dataclass], None, None]:
+    """Метод трансформации данных из SQLite"""
     for batch in extract_data(sqlite_cursor, table_name, column_names):
-        yield [table_fabric[table_name](**_rename_column(dict(row))) for row in batch]
+        yield [table_fabric[table_name](**_reform_data(dict(row))) for row in batch]
+
+
+def test_transfer(sqlite_cursor: sqlite3.Cursor, pg_cursor: ClientCursor, table: str) -> None:
+    """Метод проверки данных после загрузки в Postgres"""
+    print(f'Проверка таблицы {table}')
+    pg_column_names = [field.name for field in fields(table_fabric[table])]
+    column_names_str = ', '.join(pg_column_names)
+    query_sqlite = f'SELECT {column_names_str} FROM {table}'
+    sqlite_cursor.execute(_replace_column_name(query_sqlite))
+
+    while batch := sqlite_cursor.fetchmany(BATCH_SIZE):
+        original_batch = [table_fabric[table](**_reform_data(dict(row))) for row in batch]
+        ids = [item.id for item in original_batch]
+        pg_cursor.execute(f'SELECT * FROM content.{table} WHERE id = ANY(%s)', [ids])
+        transferred_batch = [table_fabric[table](**dict(row)) for row in pg_cursor.fetchall()]
+
+        assert len(original_batch) == len(transferred_batch)
+        assert sorted(original_batch, key=lambda item: item.id) == sorted(transferred_batch, key=lambda item: item.id)
+    print(f'ok {table}')
 
 
 def load_data(sqlite_cursor: sqlite3.Cursor, pg_cursor: ClientCursor, table_name: str) -> None:
@@ -149,3 +196,4 @@ if __name__ == '__main__':
                 load_data(sqlite_cur, pg_cur, tbl_name)
                 pg_conn.commit()
                 print(f'Таблица {tbl_name} залита')
+                test_transfer(sqlite_cur, pg_cur, tbl_name)
